939afc0a0441ca147453401dba9ad608a7e1f046
Kevin Li <kli6891@gmail.com>
2013-09-22 00:39:44 -0500

    started work on caltech-course data struct

--- js/caltech_scheduler.js
+++ js/caltech_scheduler.js
@@ -1,3 +1,79 @@
+function makeStruct(names) {
+  var names = names.split(' ');
+  var count = names.length;
+  function constructor() {
+    for (var i = 0; i < count; i++) {
+      this[names[i]] = arguments[i];
+    }
+  }
+  return constructor;
+}
+
+var CourseOptionEnum = Object.freeze({
+	ACM: {},
+	CS: {}
+});
+var CourseGradeEnum = Object.freeze({
+	EITHER: {},
+	GRADES: {}
+})
+
+var CaltechCourse = makeStruct("ID course_option course_number course_name units section prof_first_name prof_last_name subtitle annotations fixed_time times days locations grade");
+/*
+ID,
+course_option,
+course_number,
+course_name,
+units, 
+section,
+prof_first_name,
+prof_last_name,
+subtitle,
+annotations,
+fixed_time,
+times,
+days, 
+locations, 
+grades
+*/
+
+master_course_list = [
+new CaltechCourse(0,
+	CourseOptionEnum.ACM,
+	"95",
+	"Introductory Methods of Applied Mathematics",
+	12,
+	1,
+	"Pierce",
+	"Niles",
+	"",
+	[],
+	true,
+	[[11, 12], [11,12]],
+	[[1,3,5],[2]],
+	["119 KRK", "11 DWN"],
+	CourseGradeEnum.EITHER
+	),
+
+new CaltechCourse(1,
+	CourseOptionEnum.CS,
+	"156a",
+	"Learning Systems",
+	9,
+	1,
+	"Abu-Mostafa",
+	"Y",
+	"",
+	["Organization meeting, M 5-6PM at 070 MRE"],
+	false,
+	[],
+	[],
+	[],
+	CourseGradeEnum.GRADES
+	)
+]
+
+
 $(document).ready(
 	function()
 	{
@@ -15,3 +91,11 @@ $(document).ready(
 				// $( ".caltech-course").dfilter('course-name', search_term + "*.").show();
 			});
 	});
+
+
+
+// need to the functions
+// master_courses = list of classes
+// addID(id) -- function;
+// active_courses = list of id's
+// generate_event_from_id(id) -- generate a FC event corresponding to the course id.
