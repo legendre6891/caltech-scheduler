4c3ee75c08ab21b14ca983dc3a47acf6e04361e0
Kevin Li <kli6891@gmail.com>
2013-09-24 00:05:53 -0500

    added sanitized schedule

---
+++ fix.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+sed -i "/^__*/d" $1
+sed -i "/^\./d" $1
+sed -i "/Go to top/d" $1
+sed -i "/Course Offering/d" $1
+sed -i "/Units/d" $1
+sed -i "/Offering Title/d" $1
+sed -i "/Section/d" $1
+sed -i "/Instructor/d" $1
+sed -i "/Days\/Time/d" $1
+sed -i "/Location/d" $1
+sed -i "/Grade Scheme/d" $1
+sed -i "/AEROSPACE/d" $1
+sed -i "/ANTHROPOLOGY/d" $1
+sed -i "/APPLIED/d" $1
+sed -i "/APPLIED MECHANICS/d" $1
+sed -i "/APPLIED PHYSICS/d" $1
+sed -i "/ART HISTORY/d" $1
+sed -i "/ASTROPHYSICS/d" $1
+sed -i "/BIOCHEMISTRY/d" $1
+sed -i "/BIOENGINEERING/d" $1
+sed -i "/BIOLOGY/d" $1
+sed -i "/BUSINESS ECONOMICS/d" $1
+sed -i "/CHEMICAL ENGINEERING/d" $1
+sed -i "/CHEMISTRY/d" $1
+sed -i "/CIVIL ENGINEERING/d" $1
+sed -i "/COMPUTATION/d" $1
+sed -i "/COMPUTER SCIENCE/d" $1
+sed -i "/CONTROL/d" $1
+sed -i "/ECONOMICS/d" $1
+sed -i "/ELECTRICAL ENGINEERING/d" $1
+sed -i "/ENERGY SCIENCE AND TECHNOLOGY/d" $1
+sed -i "/ENGINEERING/d" $1
+sed -i "/ENGLISH/d" $1
+sed -i "/ENGLISH AS A SECOND LANGUAGE/d" $1
+sed -i "/ENVIRONMENTAL SCIENCE/d" $1
+sed -i "/FILM/d" $1
+sed -i "/FRESHMAN SEMINARS/d" $1
+sed -i "/GEOLOGY/d" $1
+sed -i "/HISTORY/d" $1
+sed -i "/HISTORY AND PHILOSOPHY OF SCIENCE/d" $1
+sed -i "/HUMANITIES/d" $1
+sed -i "/LANGUAGES/d" $1
+sed -i "/LAW/d" $1
+sed -i "/MATERIALS SCIENCE/d" $1
+sed -i "/MATHEMATICS/d" $1
+sed -i "/MECHANICAL ENGINEERING/d" $1
+sed -i "/MEDICAL ENGINEERING/d" $1
+sed -i "/MUSIC/d" $1
+sed -i "/NEUROBIOLOGY/d" $1
+sed -i "/PERFORMANCE AND ACTIVITIES/d" $1
+sed -i "/PHILOSOPHY/d" $1
+sed -i "/PHYSICAL EDUCATION/d" $1
+sed -i "/PHYSICS/d" $1
+sed -i "/POLITICAL SCIENCE/d" $1
+sed -i "/PSYCHOLOGY/d" $1
+sed -i "/SOCIAL SCIENCE/d" $1
+sed -i "/STUDY ABROAD/d" $1
+sed -i "/WRITING/d" $1
