1e1b02fbaec8a29d45be92c60e1c5899e407f77d
Kevin Li <kli6891@gmail.com>
2013-09-24 16:30:51 -0500

    Worked on Step 1 parse; added comments

--- parseme.py
+++ parseme.py
@@ -16,16 +16,37 @@ def flatten_level_one_list(xs):
 
 
 def initial_parse(current_line):
+
+	# In the current implementation, this is the *first*
+	# function that is performed on current_line.
+	# zero_parse: string -> [strings]
+	zero_parse = lambda s: s.split(' ')
+
+
+	# initial_parse_functions is a list of (func, priority)
+	# where:
+	#
+	# func `f' have the signature:
+	# f: [[strings], string] -> [[strings], string]
+	#
+	# priority is a number.
 	initial_parse_functions = [
-	[lambda s: s.split(' '), 1],
-	[lambda t, s: [map(word_number_split, t),s], 2],
-	[lambda t, s: [flatten_level_one_list(t), s], 3]
+	(lambda t, s: [map(word_number_split, t),s], 2),
+	(lambda t, s: [flatten_level_one_list(t), s], 3),
+	(lambda t, s: [map(lambda string: string.split('/'), t), s], 4),
+	(lambda t, s: [flatten_level_one_list(t), s], 5),
 	]
 
+	# Each of the func's in initial_parse_functions is run on [token, string]
+	# in order of increasing priority (1 is run first!)
+	# the input to the first is: [token, current_line]
+	initial_parse_functions = sorted(initial_parse_functions, key=lambda l: l[1])
 
-	token = initial_parse_functions[0][0](current_line)
-	[token, current_line] = initial_parse_functions[1][0](token, current_line)
-	[token, current_line] = initial_parse_functions[2][0](token, current_line)
+	token = zero_parse(current_line)
+	string = current_line
+	for (function, priority) in initial_parse_functions:
+		[token, string] = function(token, string)
+
 
 	return [token, current_line]
 
