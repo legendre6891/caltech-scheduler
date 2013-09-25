d40002f5d94eb0b3b142b13605aefb1049e04445
Kevin Li <kli6891@gmail.com>
2013-09-25 15:56:28 -0500

    successfully (AFAIK) identifies lines

--- testme.py
+++ testme.py
@@ -3,16 +3,21 @@
 
 from sys import argv
 from parseme import identify_type
-
+from pprint import pprint
 
 def main():
 
 	with open(argv[1], "r") as f:
 	    lines = f.readlines()
 
+	course_exceptions = []
 	for line in lines:
 		line = line.rstrip()
 		print identify_type(line) + "|-->>  " + line
+		# if identify_type(line) == "UNSURE":
+		# 	course_exceptions.append(line)
+
+	# pprint(list(set(course_exceptions)))
 
 if __name__ == '__main__':
 	main()
