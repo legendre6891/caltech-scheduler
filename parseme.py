2b53017bacf61fe84835bd788dea55ca5c25d27e
Kevin Li <kli6891@gmail.com>
2013-09-24 19:19:50 -0500

    more token recognition

--- parseme.py
+++ parseme.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
+
 import re
 from sys import argv
 
 
 def word_number_split(word):
-	match = re.match(r"([a-z]+)([0-9]+)", word, re.I)
+	match = re.match(r"([a-z]+)([0-9]+[A-Z]*$)", word, re.I)
 	if match:
 		items = match.groups()
 	else:
@@ -13,6 +16,34 @@ def word_number_split(word):
 
 def flatten_level_one_list(xs):
 	return [a for b in xs for a in b]
+
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def is_course_name(token, string):
+	options = ['Ae', 'An', 'ACM', 'AM', 'APh', 'Art', 'Ay', 'BMB', 'BE', 'Bi',
+	'BEM', 'Ch', 'CHE', 'CDS', 'CNS', 'CS', 'CE', 'Ec', 'ESL', 'EE', 'E', 'EN',
+	'ESE', 'F', 'FS', 'Ge', 'H', 'HPS', 'Hum', 'L', 'Law', 'Ma', 'MS', 'ME',
+	'Mu', 'Pl', 'PE', 'Ph', 'PS', 'PA', 'Psy', 'SS', 'SA', 'Wr']
+	return token[0] in options
+
+def is_units(token, string):
+	s = string.split('-')
+	return string == '+' or (len(s)==3 and filter(is_int, s)==s)
+
+def is_section(token, string):
+	return is_int(string)
+
+
+def is_professor_name(token, string):
+	return token[-2][-1] == ',' and token[-1].isupper()
+
+
 
 
 def initial_parse(current_line):
